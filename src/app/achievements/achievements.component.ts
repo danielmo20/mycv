c7c0656c48e2d3e05f28eb04ca3f0ceaf6204794
Daniel Moscarella <daniel.moscarella@bbva.com>
2020-08-16 22:01:23 -0500

    added agnular material and flex-layout, completed modules: about, achievements

--- src/app/achievements/achievements.component.ts
+++ src/app/achievements/achievements.component.ts
@@ -7,8 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AchievementsComponent implements OnInit {
 
-  constructor() { }
+	achievements = [];
 
+  constructor() { 
+  
+	this.achievements = [
+		{
+			"text": "Application Manager BBVA móvil",
+			"when": "2020 - present",
+			"where": "BBVA"
+		},
+		{
+			"text": "Lider de equipo con diversos proyectos instalados en producción",
+			"when": "2016 - 2019",
+			"where": "BBVA"
+		},
+		{
+			"text": "Mantenimiento evolutivo sobre la plataforma bancaria Axon para Neurona",
+			"when": "2015-2016",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Desarrollador del Portal institucional AMR",
+			"when": "2016",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Desarrollador plataforma de académica para Weisheit basado en MEAN.",
+			"when": "2015",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Arquitecto y desarrollador del portal transaccional de Coopcentral \"Multiportal\"",
+			"when": "2014",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Arquitecto y desarrollador del sistema de Normalización de Auditoría del SIIDJ (NAS) para Deceval SA.",
+			"when": "2013",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Arquitecto y desarrollador portal web de ventas y portal de administración de MovilRed",
+			"when": "2009-2012",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Desarrollador Plataforma transaccional de Solidda Group y Technologistics.",
+			"when": "2013",
+			"where": "KOGHI"
+		},
+		{
+			"text": "Implentación sistema de integración continua con Jenkins, Koghi SAS",
+			"when": "2013",
+			"where": "KOGHI"
+		},
+	];
+  }  
+  
   ngOnInit(): void {
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { AchievementsComponent } from './achievements/achievements.component';
 import { SocialComponent } from './social/social.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import {MatTabsModule} from '@angular/material/tabs';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { SocialComponent } from './social/social.component';
     SocialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	MatToolbarModule,
+	MatCardModule,
+	MatTabsModule,
+	FlexLayoutModule,
+	MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
